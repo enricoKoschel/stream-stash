c7679a365cdc7d9ea2a5a171e08e8a8d2c31e16c
Enrico Koschel <enricokoschel02@gmail.com>
2024-01-25 21:47:56 +0100

    Persist login in local storage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuthStore } from 'stores/authStore';
+import { LocalStorage } from 'quasar';
+
+const authStore = useAuthStore();
+
+const initialSessionId = LocalStorage.getItem<string>('sessionId');
+if (initialSessionId) {
+  authStore.sessionId = initialSessionId;
+}
+
+authStore.$subscribe((mutation, state) => {
+  LocalStorage.set('sessionId', state.sessionId);
+});
+</script>
 
 <template>
   <!-- TODO: Popup error, global error value in separate error store -->
